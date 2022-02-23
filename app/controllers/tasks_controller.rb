fcaf0e1ba8bfcb663101381be247b25cddffdb32
andreia tarifa <andreia.p.tarifa@gmail.com>
2022-02-23 09:01:04 -0300

    challenge nao finalizado

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,38 @@
 class TasksController < ApplicationController
+
+  # See all tasks
   def index
+    @tasks = Task.all
   end
 
+  # See details about one task
   def show
   end
 
-  rails g migration Set
+  # Create a task
+  def create
+    # como vou usar o input do user para criar a nova task, preciso passar como argumento de #new o params de task
+    @task = Task.new(task_params)
+    @task.save # como vou salvar no db, tbm preciso adicionar o metodo #save na task criada
+    # para que o user nao pense que a pag quebrou, preciso redireciona-lo para
+    # a pag principal de tasks apos a task ter sido criada com sucesso
+  end
+
+  # Update a task
+  # Posso quebrar o update em 2 etapas:
+  # 1°: exibir a task para o user -> rota get, metodo 'edit'
+  # 2°: deixar que ele atualize as infos que quiser(e que eu permitir) -> patch, metodo 'update'
+  def edit
+  end
+
+  def update
+  end
+
+  # Destroy a task
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed) # preciso inserir como argumento de #permit os campos
+    # que vou permitir que sejam alterados quando o user cria uma nova task
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-get 'tasks', to: 'tasks#index'
+  get 'tasks', to: 'tasks#index'
+
+  post 'tasks/create', to: 'tasks#create'
+
+  get 'tasks/:id/edit', to: 'tasks#edit',  as: :edit_task # preciso criar um edit como um get, pois irei
+  # exibir as infos da task e o seu respectivo id para depois deixar
+  # que o user a atualize(com o metodo #update,que, este sim, sera um patch)
+
+  get 'tasks/:id', to: 'tasks#show', as: :task
+
+
 end
